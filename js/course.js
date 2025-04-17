ab409791d87f101c91e06979bbb7a4e7907b4b26
Nico <nicolas.melia@mi.unc.edu.ar>
2025-04-17 01:40:30 -0300

    new changes

--- js/course.js
+++ js/course.js
@@ -7,7 +7,7 @@ var courseParam = urlParam.get('course');
 var course = courses.find(course => course.url.includes(courseParam));
 var btnContact = document.querySelector('#btn-contact');
 btnContact.addEventListener('click', function() {
-    window.location.href = course.contactForm;
+    window.location.href = "https://api.whatsapp.com/send/?phone=%2B5493512445290&text&type=phone_number&app_absent=0";
 });
 
 courseImg.src = course.icon;
@@ -29,11 +29,14 @@ for(var req in course.technicalRequirements){
     ulTechReq.appendChild(li);
 }
 
-for(var key in course.description){
-    if(typeof(course.description[key]) == 'string'){
-        description.innerText = course.description[key];
-    }else{
-        var title = document.createElement('h5');
+for (var key in course.description) {
+    if (typeof(course.description[key]) == 'string') {
+        var paragraph = document.createElement('p'); // Create a paragraph for each string
+        paragraph.innerText = course.description[key];
+        description.appendChild(paragraph); // Append it to the description
+        console.log(course.description[key]);
+    } else {
+        var title = document.createElement('h5'); // H5
         title.innerText = course.description[key].title;
         description.appendChild(title);
         var ul = document.createElement('ul');
