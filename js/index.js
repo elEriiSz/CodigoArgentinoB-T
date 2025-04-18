b02fd42f80bdf927ac7ff240f0f0092ad8d5d634
Nico <nicolas.melia@mi.unc.edu.ar>
2025-04-17 22:50:13 -0300

    btn trial

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var btnIg = document.querySelector(".icon-ig");
 var btnWp = document.querySelector(".icon-wp");
+var btnTrial = document.querySelector("#btn-trial");
 
 btnIg.addEventListener("click", function () {
   window.open("https://www.instagram.com/codigo_argentino/", "_blank");
@@ -7,4 +8,8 @@ btnIg.addEventListener("click", function () {
 
 btnWp.addEventListener("click", function () {
   window.open("https://api.whatsapp.com/send/?phone=%2B5493512445290&text&type=phone_number&app_absent=0", "_blank");
+});
+
+btnTrial.addEventListener("click", function () {
+  window.open("https://api.whatsapp.com/send?phone=+5493512445290&text=Hola!%20Quiero%20coordinar%20una%20clase%20de%20prueba%20%F0%9F%98%8A", "_blank");
 });
