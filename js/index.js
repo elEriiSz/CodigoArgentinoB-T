cb96d853589f5bebd1a2439000d17960032db410
Nico <nicolas.melia@mi.unc.edu.ar>
2025-04-20 00:09:02 -0300

    sale added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var btnIg = document.querySelector(".icon-ig");
 var btnWp = document.querySelector(".icon-wp");
-var btnTrial = document.querySelector("#btn-trial");
+var btnTrial = document.querySelectorAll(".btn-trial");
 
 btnIg.addEventListener("click", function () {
   window.open("https://www.instagram.com/codigo_argentino/", "_blank");
@@ -10,6 +10,14 @@ btnWp.addEventListener("click", function () {
   window.open("https://api.whatsapp.com/send/?phone=%2B5493512445290&text&type=phone_number&app_absent=0", "_blank");
 });
 
-btnTrial.addEventListener("click", function () {
-  window.open("https://api.whatsapp.com/send?phone=+5493512445290&text=Hola!%20Quiero%20coordinar%20una%20clase%20de%20prueba%20%F0%9F%98%8A", "_blank");
+for(var btn of btnTrial){
+  btn.addEventListener("click", function () {
+    window.open("https://api.whatsapp.com/send?phone=+5493512445290&text=Hola!%20Quiero%20coordinar%20una%20clase%20de%20prueba%20%F0%9F%98%8A", "_blank");
+  });
+}
+
+window.addEventListener("load", function () {
+  const toastElement = document.getElementById('liveToast');
+  const toast = new bootstrap.Toast(toastElement);
+  toast.show();
 });
