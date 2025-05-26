fa2c9b0c86a68ba6fe138692a25dac49a868208c
Nico <nicolas.melia@mi.unc.edu.ar>
2025-05-25 22:53:15 -0300

    hotsale

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-var btnIg = document.querySelector(".icon-ig");
-var btnWp = document.querySelector(".icon-wp");
-
-btnIg.addEventListener("click", function () {
-  window.open("https://www.instagram.com/codigo_argentino/", "_blank");
-});
-
-btnWp.addEventListener("click", function () {
-  window.open("https://api.whatsapp.com/send/?phone=%2B5493512445290&text&type=phone_number&app_absent=0", "_blank");
-});
-
 window.addEventListener("load", function () {
   const toastElement = document.getElementById('liveToast');
   const toast = new bootstrap.Toast(toastElement);
