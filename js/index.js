485e9bc015824fe84341b48d9c74818e5632717f
Nyx <elerii098@gmail.com>
2025-06-09 19:00:08 -0300

    Agregado de funcion que hace que el contenido de los cursos este alineado sin importar que el usuario haga zoom

--- js/index.js
+++ js/index.js
@@ -10,3 +10,24 @@ btnClose.addEventListener("click", function () {
   const toast = new bootstrap.Toast(toastElement);
   toast.hide();
 });
+
+function changeParagraphsMinHeight() {
+  const paragraphs = document.querySelectorAll('.course-content p');
+  let max_height = 0;
+  for (var i = 0; i < paragraphs.length; i++) {
+    console.log(paragraphs[i].innerText);
+    console.log(paragraphs[i].offsetHeight);
+    if(paragraphs[i].offsetHeight > max_height){
+      max_height = paragraphs[i].offsetHeight;
+    };
+  };
+  for (var i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.minHeight = `${max_height}px`;
+    console.log(paragraphs[i].innerText);
+    console.log(paragraphs[i].style.minHeight);
+  };
+};
+changeParagraphsMinHeight();
+window.addEventListener('resize', function() {
+  changeParagraphsMinHeight();
+});
